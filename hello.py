410403a958362b74239f458758fd57c22ffc9742
Connor Blakeney <connorblakeney@yahoo.com>
2021-03-04 14:39:26 -0600

    started webscraping module

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-import re
+# import re
 
-phoneNumString = "Call me at 415-555-1111 today or 415-555-2222 tomorrow."
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
-mo = phoneNumRegex.findall(phoneNumString)
-print(mo)
+# phoneNumString = "Call me at 415-555-1111 today or 415-555-2222 tomorrow."
+# phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# mo = phoneNumRegex.findall(phoneNumString)
+# print(mo)
+
+for i in range(100, 0, -2):
+    print(i)
---
+++ webscraping.py
@@ -0,0 +1,4 @@
+import webbrowser, sys
+
+webbrowser.open('https://automatetheboringstuff.com')
+sys.argv
