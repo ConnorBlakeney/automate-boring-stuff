9cbd49685c8d1e9fb553a038e5799da67bd8980f
Connor Blakeney <connorblakeney@yahoo.com>
2021-03-10 10:14:24 -0600

    played around with gui python

--- gui.py
+++ gui.py
@@ -1,4 +1,13 @@
-import pyautogui
+# import pyautogui
 
-print(pyautogui.position())
-pyautogui.moveTo(100, 100, duration=5)
+# print(pyautogui.position())
+# pyautogui.moveTo(100, 100, duration=5)
+
+# thought_of_the_day = "If programmers write programs, who writes programmers?"
+# print(thought_of_the_day[21:29])
+
+# message = "practice"
+# print(message.capitalize())
+
+message = "practice makes perfect"
+print(message.split("m"))
